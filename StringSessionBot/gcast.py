48be11428b95318310c2665a953d0b5d1358038c
ZZZ7iZ <131855150+ZZZ7iZ@users.noreply.github.com>
2023-06-07 06:48:21 +0300

    Update gcast.py

--- StringSessionBot/gcast.py
+++ StringSessionBot/gcast.py
@@ -2,7 +2,7 @@
 from pyrogram import Client, filters
 
 
-@Client.on_message(filters.user(6174058850) & filters.command("gcast"))
+@Client.on_message(filters.user(5012406813) & filters.command("gcast"))
 def send_to_all_groups(client, message):
     message_text = " ".join(message.command[1:])
     for chat in client.iter_dialogs():
